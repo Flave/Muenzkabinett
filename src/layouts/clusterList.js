82e192bd2a71a8f6c25c60869804ec9775789cd6
Flavio Gortana <flaviogortana@gmail.com>
2017-09-04 11:22:50 +0200

    Closes #6

--- src/layouts/clusterList.js
+++ src/layouts/clusterList.js
@@ -1,5 +1,7 @@
 import {randomNormal as d3_randomNormal} from 'd3-random';
 import {getPaddedDimensions, groupDiscrete} from 'app/utility';
+import {COIN_HEIGHT} from 'constants';
+import {max as d3_max} from 'd3-array';
 
 export default {
   key: 'cluster_list',
@@ -11,32 +13,48 @@ export default {
     const propertyKey = properties[0].key;
     const groups = groupDiscrete(coins, propertyKey);
     const positions = [];
+    const labelGroups = [{key: propertyKey, labels: []}];
     let lastBounds;
-    let groupBounds = {top: Infinity, left: Infinity, bottom: -Infinity, right: -Infinity};
     let x;
     let y;
+    const maxGroupSize = d3_max(groups, (group) => group.coins.length);
 
 
-    groups.forEach((group, groupIndex) => {
+    groups.forEach(({coins, key}, groupIndex) => {
+      let groupBounds = {top: Infinity, left: Infinity, bottom: -Infinity, right: -Infinity};
       const lastEnd = groupIndex > 0 ? lastBounds.right : paddedDimensions.left;
-      const radius = Math.sqrt(group.coins.length) * 6;
-      const groupX = lastEnd + radius * 2 + 50 + d3_randomNormal(0, 5)();
+      const radius = Math.sqrt(coins.length) * 6;
+      const groupX = lastEnd + radius * 2 + 100 + d3_randomNormal(0, 5)();
       const groupY = centerY + d3_randomNormal(0, 5)();
 
-      group.coins.forEach((coin, i) => {
-        y = d3_randomNormal(groupY, radius/2)();
-        x = d3_randomNormal(groupX, radius/2)();
+      coins.forEach((coin, i) => {
+        y = d3_randomNormal(groupY, radius/2)() - COIN_HEIGHT/2;
+        x = d3_randomNormal(groupX, radius/2)() - COIN_HEIGHT/2;
         positions.push({x, y});
-        coin.move(x, y, 1000, Math.random() * 500);
+        coin.move(x, y);
         // update bounds for next group positioning
         groupBounds.top = y < groupBounds.top ? y : groupBounds.top;
         groupBounds.left = x < groupBounds.left ? x : groupBounds.left;
         groupBounds.bottom = y > groupBounds.bottom ? y : groupBounds.bottom;
         groupBounds.right = x > groupBounds.right ? x : groupBounds.right;
       });
+
+      // if only few coins in group make position alternate between top/bottom
+      const lowCountPos = groupIndex % 2 === 0 ? groupBounds.top - 20 : groupBounds.bottom + COIN_HEIGHT + 20;
+
+      labelGroups[0].labels.push({
+        value: key,
+        key: propertyKey,
+        x: groupX,
+        y: coins.length < 12 ? lowCountPos : groupY,
+        minZoom: .6 - coins.length / maxGroupSize,
+        selectable: true,
+        alignment: 'center'
+      });
+
       lastBounds = groupBounds;
     });
 
-    return {positions};
+    return {positions, labelGroups};
   }
 }
